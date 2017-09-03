31f70fdc98c49208f673baf0f27a2931df5cdb98
Jacek Kulis <jacek.jk.kulis@gmail.com>
2017-09-03 15:18:45 +0200

    add config file

---
+++ src/Config/config.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * This file contains configuration informations
+ */
+
+return array(
+    'apiKey' => 'AAAAC31dYQA:APA91bF_Qg4PpWfUYVSgu6sKMYhX7zKgCCVgKSbq-qf7hRy3Y4-b8TssEAc2zveYNyyuClQIOTb9aMYx6u9muDl7j63EswMEu2JsWQplV4HQooMQljs9esKMsNOaY1RMi_dBXy1WjI15',
+    'androidDeviceToken' => 'f0Dp87lCBAE:APA91bHyXUUeOQT5zfcaGaRd9r_NXAQQX_zKs5NpHduE8ChT3zaf4uWPWBJWJhWO4KPgozv9QEV8Ol3fBIUIgnY_-9zPxTOwWyOYvjW8CzwvPKANhhkaX5vco45M_sbu6O9xNT7vblvr',
+);
+
+?>
+

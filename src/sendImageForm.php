db3b326358e299d9bc0e556c9abefe40e3566124
Jacek Kuliś <jacek.jk.kulis@gmail.com>
2017-10-28 14:06:56 +0200

    sending notification with file name

--- src/sendImageForm.php
+++ src/sendImageForm.php
@@ -17,8 +17,40 @@ if (isset($_POST['submitImage'])){
     if (isset($_FILES['data'])){
         $config = new incognito\Config\Configuration();
         $image_uploader = new \incognito\ImageUploader($_FILES['data']);
-        echo $image_uploader->result;
+
+        if ($image_uploader->isUploaded()){
+            echo 'File uploaded';
+            echo $image_uploader->result;
+
+            // send fcm with url
+            $config = new incognito\Config\Configuration();
+            $client = new incognito\PhpFirebaseCloudMessaging\FCMClient();
+            $client->setApiKey($config->getApiKey());
+            $client->injectGuzzleHttpClient(new \GuzzleHttp\Client());
+
+            $topic = 'notifications';
+
+            $message = new incognito\PhpFirebaseCloudMessaging\Message();
+            $message->setPriority('high');
+            $message->addRecipient(new incognito\PhpFirebaseCloudMessaging\Recipient\Topic($topic));
+            $message
+                ->setNotification(new incognito\PhpFirebaseCloudMessaging\Notification('Obtained picture', $image_uploader->getTargetFile()))
+                ->setData(['key' => 'value'])
+            ;
+
+            try {
+                $response = $client->send($message);
+                echo 'Notification is sent successfully to topic "'.$topic. '". '.$response->getBody()->getContents();
+            } catch (Exception $ex) {
+                echo 'Error: ' .$ex->getMessage();
+            }
         }
+        else {
+            echo 'Error, not uploaded.';
+            echo $image_uploader->result;
+        }
+
+    }
     else {
         echo 'File not set';
     }
